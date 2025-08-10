5a556c47c88286e8beae0caed1570783d13bbd10
mohamed9278443 <sidiyacheikh2023@gmail.com>
2025-08-10 22:14:46 +0000

    Update MainVideo.tsx

--- src/components/MainVideo.tsx
+++ src/components/MainVideo.tsx
@@ -92,7 +92,7 @@ export const MainVideo: React.FC<MainVideoProps> = ({ videoUrl, className = '' }
       <div className={`w-full max-w-4xl mx-auto ${className}`}>
         <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
           <iframe
-            src={getYouTubeEmbedUrl(https://youtu.be/K06DUDGkDFc?si=-xhRsWl6-iKyBPMs)}
+            src={getYouTubeEmbedUrl(videoUrl)}
             className="absolute top-0 left-0 w-full h-full rounded-lg"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
